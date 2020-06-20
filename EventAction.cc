9a19964184139d583fd37fe4247698b40c73921d
mengji890 <66791697+mengji890@users.noreply.github.com>
2020-06-20 17:30:08 +0100

    Create EventAction.cc

---
+++ EventAction.cc
@@ -0,0 +1,35 @@
+#include "EventAction.hh"
+#include "RunAction.hh"
+
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+EventAction::EventAction(RunAction* runAction)
+: G4UserEventAction(),
+  fRunAction(runAction),
+  fNumber(0.)
+{} 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+EventAction::~EventAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void EventAction::BeginOfEventAction(const G4Event*)
+{    
+  fNumber = 0.;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void EventAction::EndOfEventAction(const G4Event*)
+{   
+  // accumulate numbers in run action 
+  fRunAction->AddNumb(fNumber);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
