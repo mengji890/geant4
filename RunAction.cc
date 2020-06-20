39f21a97dd9ce848617ec97c05c4e3ddea41af89
mengji890 <66791697+mengji890@users.noreply.github.com>
2020-06-20 18:33:06 +0100

    Create RunAction.cc

---
+++ RunAction.cc
@@ -0,0 +1,120 @@
+#include "RunAction.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "DetectorConstruction.hh"
+
+
+#include "G4RunManager.hh"
+#include "G4Run.hh"
+#include "G4AccumulableManager.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+RunAction::RunAction()
+: G4UserRunAction(),
+  fNumber(0.),
+  
+{ 
+ 
+
+  // Register accumulable to the accumulable manager
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->RegisterAccumulable(fNumber);
+ 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+RunAction::~RunAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run*)
+{ 
+  // inform the runManager to save random number seed
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+
+  // reset accumulables to their initial values
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Reset();
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* run)
+{
+  G4int nofEvents = run->GetNumberOfEvent();
+  if (nofEvents == 0) return;
+
+  // Merge accumulables 
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Merge();
+
+ 
+  G4double numb  = fNumber.GetValue();
+ 
+  
+   
+
+  const DetectorConstruction* detectorConstruction
+   = static_cast<const DetectorConstruction*>
+     (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+
+
+  // Run conditions
+  //  note: There is no primary generator action object for "master"
+  //        run manager for multi-threaded mode.
+  const PrimaryGeneratorAction* generatorAction
+   = static_cast<const B1PrimaryGeneratorAction*>
+     (G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+  G4String runCondition;
+  if (generatorAction)
+  {
+    const G4ParticleGun* particleGun = generatorAction->GetParticleGun();
+    runCondition += particleGun->GetParticleDefinition()->GetParticleName();
+    runCondition += " of ";
+    G4double particleEnergy = particleGun->GetParticleEnergy();
+    runCondition += G4BestUnit(particleEnergy,"Energy");
+  }
+        
+  // Print
+  //  
+  if (IsMaster()) {
+    G4cout
+     << G4endl
+     << "--------------------End of Global Run-----------------------";
+  }
+  else {
+    G4cout
+     << G4endl
+     << "--------------------End of Local Run------------------------";
+  }
+  
+  G4cout
+     << G4endl
+     << " The run consists of " << nofEvents << " "<< runCondition
+     << G4endl
+     << " Cumulated numbers per run, in scoring volume : " 
+     << numb
+     << G4endl
+     << "------------------------------------------------------------"
+     << G4endl
+     << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::AddEdep(G4double numb)
+{
+  fNumber  += numb;
+  
+}
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
