c816ec03cf47ec84b48da4d70eefe3dfc78d2c60
mengji890 <66791697+mengji890@users.noreply.github.com>
2020-06-21 10:59:30 +0100

    Update EventAction.hh

--- include/EventAction.hh
+++ include/EventAction.hh
@@ -18,11 +18,11 @@ class EventAction : public G4UserEventAction
     virtual void BeginOfEventAction(const G4Event* event);
     virtual void EndOfEventAction(const G4Event* event);
 
-    void AddEdep(G4double numb) { fNumb += numb; }
+    void AddNumb(G4double numb) { fNumber += numb; }
 
   private:
     RunAction* fRunAction;
-    G4double     fNumb;
+    G4double     fNumber;
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
